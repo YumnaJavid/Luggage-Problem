c2c84ae473550a5e3d6504d6bd1b77e1a71750b0
YumnaJavid <57884300+YumnaJavid@users.noreply.github.com>
2019-11-30 21:44:34 +0500

    Login

--- LUGGAGE/LUGGAGE/Form2.cs
+++ LUGGAGE/LUGGAGE/Form2.cs
@@ -10,23 +10,11 @@ using System.Windows.Forms;
 
 namespace LUGGAGE
 {
-    public partial class Form1 : Form
+    public partial class Form2 : Form
     {
-        public Form1()
+        public Form2()
         {
             InitializeComponent();
-        }
-
-        private void pictureBox1_Click(object sender, EventArgs e)
-        {
-           
-         
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-          
-            BackColor = Color.LightPink;
         }
     }
 }
