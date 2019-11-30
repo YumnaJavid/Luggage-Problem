c403f9396ba573b0aead9ab0c58a06eb74eaec2f
YumnaJavid <57884300+YumnaJavid@users.noreply.github.com>
2019-11-30 16:47:38 +0500

    gui

--- LUGGAGE/LUGGAGE/Program.cs
+++ LUGGAGE/LUGGAGE/Program.cs
@@ -4,16 +4,16 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace WindowsFormsApplication1
+namespace LUGGAGE
 {
     static class Program
     {
-       
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
         [STAThread]
         static void Main()
         {
-            string filepath = " ";
-            Console.ReadLine();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
